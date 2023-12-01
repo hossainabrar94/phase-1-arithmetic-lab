cc5cc7d70d40b904aae0223625b99e2b7a118730
Abrar <hossainabrar@aol.com>
2023-11-30 20:56:48 -0500

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
-// Write your code here
+/*function multiply(num1, num2){
+    if(typeof num1 === "number"){
+        if(typeof num2 === 'number'){
+            num1 = 31;
+            num2 = 2;
+            return num1*num2
+        }
+    }
+}
+*/
+let multiply, num1=31, num2=2, random, mod, num3=19, num4=5, max
+multiply = num1*num2;
+random = Math.floor(Math.random()*10);
+mod = num3%num4;
+max = Math.max(1,19,20);
